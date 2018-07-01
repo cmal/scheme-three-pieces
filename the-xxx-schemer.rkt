55b754be393a37a9989f023770c75785130018b8
Yu Zhao <zyzy5730@163.com>
2018-07-01 16:40:09 +0800

    rename

--- the-xxx-schemer.rkt
+++ the-xxx-schemer.rkt
@@ -892,10 +892,40 @@
 (insertL 'z 'a '(a c b a d e a))
 (insertR 'z 'a '(a c b a d e a))
 
+(define seqS
+  (lambda (new old l)
+    (cons new l)))
+
+(seqS 'a 'b '(c))
+(trace subst)
+(trace insert-g)
+(trace seqS)
+(define subst
+  (insert-g seqS))
+
+(lambda (new old l)
+      (cond
+        ((null? l) '())
+        ((eq? old (car l)) (seqS new old (recur new old (cdr l))))
+        (else (cons (car l) (recur new old (cdr l))))))
+
+(subst 'g 'e '(a b c d e f))
+(subst 'g 'e '())
+
+
+(define yyy
+  (lambda (a l)
+    ((insert-g seqrem) #f a l)))
+
+(define seqrem
+  (λ (new old l)
+    l))
+
+(yyy 'sausage '(pizza with sausage and bacon))
+
 (define atom-to-function
   (λ (atom)
-    
-    ))
+    atom))
 
 (define operator
   (λ (nexp)
@@ -1337,3 +1367,343 @@ caviar
     ((null? l) 0)
     (else (add1 (eternity (cdr l))))))
 
+
+(((lambda (mk-length)
+          (mk-length mk-length))
+  (λ (mk-length)
+    (lambda (l)
+      (cond
+        ((null? l) 0)
+        (else (add1
+               ((mk-length eternity)
+                (cdr l))))))))
+ '(apples))
+
+((lambda (l)
+   (cond
+     ((null? l) 0)
+     (else (add1
+            ((eternity eternity)
+             (cdr l)))))) '())
+
+
+
+(((lambda (mk-length)
+    (mk-length mk-length))
+  (lambda (mk-length)
+    ((lambda (length)
+       (lambda (l)
+         (cond
+           ((null? l) 0)
+           (else (add1 (length (cdr l)))))))
+     (mk-length mk-length))))
+ '(apples))
+
+
+(((lambda (length)
+    (lambda (l)
+      (cond
+        ((null? l) 0)
+        (else (add1 (length (cdr l)))))))
+  ((lambda (mk-length)
+    ((lambda (length)
+       (lambda (l)
+         (cond
+           ((null? l) 0)
+           (else (add1 (length (cdr l)))))))
+     (mk-length mk-length)))
+   (lambda (mk-length)
+    ((lambda (length)
+       (lambda (l)
+         (cond
+           ((null? l) 0)
+           (else (add1 (length (cdr l)))))))
+     (mk-length mk-length)))))
+ '(apples))
+
+
+(define Y
+  (λ (le)
+    ((lambda (f) (f f))
+     (λ (f)
+       (le (λ (x) ((f f) x)))))))
+
+(define new-entry build)
+
+(define lookup-in-entry
+  (λ (name entry entry-f)
+    (lookup-in-entry-help name
+                          (first entry)
+                          (second entry)
+                          entry-f)))
+
+(define lookup-in-entry-help
+  (λ (name names values entry-f)
+    (cond
+      ((null? names) (entry-f name))
+      ((equal? name (car names)) (car values))
+      (else (lookup-in-entry-help
+             name (cdr names) (cdr values) entry-f)))))
+
+(lookup-in-entry 'entree
+                 '((appetizer entree beverage)
+                   (food tastes good))
+                 (λ (x) x))
+
+(lookup-in-entry '(pate boeuf vin)
+                 '(((appetizer entree beverage) (pate boeuf vin))
+                   ((beverage dessert) ((food is) (number one with us))))
+                 (λ (x) x))
+
+(lookup-in-entry '(pate boeuf vin)
+                 '(((appetizer entree beverage) (pate boeuf vin))
+                   ((beverage dessert) ((food is) (number one with us))))
+                 (λ (x) x))
+
+(define extend-table cons)
+
+(define lookup-in-table
+  (λ (name table table-f)
+    (cond
+      ((null? table) (table-f name))
+      (else (lookup-in-entry name
+                             (car table)
+                             (lambda (name)
+                               (lookup-in-table name
+                                                (cdr table)
+                                                table-f)))))))
+
+(lookup-in-table
+  'entree
+  '(((appetizer entree beverage) (pate boeuf vin))
+    ((beverage dessert) ((food is) (number one with us))))
+  (λ (x) x))
+
+(lookup-in-table
+  'entree
+  '(((entree dessert) (spaghetti spumoni))
+    ((appetizer entree beverage) (food tastes good)))
+  (λ (x) x))
+
+(car (quote (a b c)))
+
+(value (car (quote (a b c))))
+(value (quote (car (quote (a b c))))) ;; ???
+
+(value '(add1 6))
+
+((λ (nothing)
+   (cons nothing '()))
+ '(from nothing comes something))
+
+(cons '(from nothing comes something) '())
+
+((lambda (nothing)
+   (cond
+     (nothing 'something)
+     (else 'nothing)))
+ #t)
+
+;; (type 6)
+
+(value car)
+
+
+;; 6 types:
+;; *const
+;; *quote
+;; *identifier
+;; *lambda
+;; *cond
+;; *application.
+
+(define atom-to-action
+  (λ (e)
+    (cond
+      ((number? e) *const)
+      ((eq? e #t) *const)
+      ((eq? e #f) *const)
+      ((eq? e 'cons) *const)
+      ((eq? e 'car) *const)
+      ((eq? e 'cdr) *const)
+      ((eq? e 'null?) *const)
+      ((eq? e 'eq?) *const)
+      ((eq? e 'atom?) *const)
+      ((eq? e 'zero?) *const)
+      ((eq? e 'add1) *const)
+      ((eq? e 'sub1) *const)
+      ((eq? e 'number?) *const)
+      (else *identifier))))
+
+(define list-to-action
+  (λ (e)
+    (cond
+      ((atom? (car e)) (cond
+                         ((eq? (car e) 'quote) *quote)
+                         ((eq? (car e) 'lambda) *lambda)
+                         ((eq? (car e) 'cond) *cond)
+                         (else *application)))
+      (else *application))))
+
+(define expression-to-action
+  (λ (e)
+    (cond
+      ((atom? e) (atom-to-action e))
+      (else (list-to-action e)))))
+
+(define meaning
+  (λ (e table)
+    ((expression-to-action e) e table)))
+
+(define value
+  (λ (e)
+    (meaning e '())))
+
+(define *const
+  (λ (e table)
+    (cond
+      ((number? e) e)
+      ((eq? e #t) #t)
+      ((eq? e #f) #f)
+      (else (build (quote primitive) e)))))
+
+(define *quote
+  (λ (e table)
+    (text-of e)))
+
+(define text-of second)
+
+(define *identifier
+  (lambda (e table)
+    (lookup-in-table e table initial-table)))
+
+(define initial-table
+  (λ (name)
+    (car '())))
+
+(define *lambda
+  (λ (e table)
+    (build 'non-primitive
+           (cons table (cdr e)))))
+
+(define table-of first)
+(define formals-of second)
+(define body-of third)
+
+(define else?
+  (λ (x)
+    (cond
+      ((atom? x) eq? x 'else)
+      (else #f))))
+
+(define question-of first)
+(define answer-of second)
+
+(define evcon
+  (λ (lines table)
+    (cond
+      ((else? (question-of (car lines)))
+       (meaning (answer-of (car lines)) table))
+      ((meaning (question-of (car lines)) table)
+       table)
+      (else (evcon (cdr lines) table)))))
+
+(define cond-lines-of cdr)
+
+(define *cond
+  (λ (e table)
+    (evcon (cond-lines-of e) table)))
+
+(*cond
+ '(cond
+    (coffee party)
+    (else klatsch))
+ '(((coffee) (#t))
+   ((klatsch party) (5 (6)))))
+
+(define evlis
+  (lambda (args table)
+    (cond
+      ((null? args) '())
+      (else
+       (cons (meaning (car args) table)
+             (evlis (cdr args) table))))))
+
+(define *application
+  (λ (e table)
+    (apply
+     (meaning (function-of e) table)
+     (evlis (arguments-of e) table))))
+
+(define function-of car)
+
+(define arguments-of cdr)
+
+(define primitive?
+  (λ (l)
+    (eq? (first l) (quote primitive))))
+
+(define non-primitive?
+  (λ (l)
+    (eq? (first l) (quote non-primitive))))
+
+(define apply
+  (λ (fun vals)
+    (cond
+      ((primitive? fun)
+       (apply-primitive
+        (second fun) vals))
+      ((non-primitive? fun)
+       (apply-closure
+        (second fun) vals)))))
+
+(define :atom?
+  (λ (x)
+    (cond
+      ((atom? x) #t)
+      ((null? x) #f)
+      ((eq? (car x) 'primitive) #t)
+      ((eq? (car x) 'non-primitive?) #t)
+      (else #f))))
+
+(define apply-primitive
+  (λ (name vals)
+    (cond
+      ((eq? name 'cons)
+       (cons (first vals) (second vals)))
+      ((eq? name 'car)
+       (car (first vals)))
+      ((eq? name 'cdr)
+       (cdr (first vals)))
+      ((eq? name 'null?)
+       (null? (first vals)))
+      ((eq? name 'eq?)
+       (eq? (first vals) (second vals)))
+      ((eq? name 'atom?)
+       (:atom? (first vals)))
+      ((eq? name 'zero?)
+       (zero? (first vals)))
+      ((eq? name 'add1)
+       (add1 (first vals)))
+      ((eq? name 'sub1)
+       (sub1 (first vals)))
+      ((eq? name 'number?)
+       (number? (first vals))))))
+
+(define apply-closure
+  (λ (closure vals)
+    (meaning (body-of closure)
+             (extend-table
+              (new-entry
+               (formals-of closure)
+               vals)
+              (table-of closure)))))
+
+(apply-closure
+ '((((u v w)
+     (1 2 3))
+    ((x y z)
+     (4 5 6)))
+   (x y)
+   (cons z x))
+ '((a b c) (d e f)))
